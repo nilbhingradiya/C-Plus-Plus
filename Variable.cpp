1efb237535e4abb89d3bca798eb12813c4ebc162
Nil Bhingradiya <70262861+nilbhingradiya@users.noreply.github.com>
2020-09-17 11:30:52 +0530

    Create Variable.cpp

---
+++ Variable.cpp
@@ -0,0 +1,18 @@
+//Find Size Of a Variable.
+
+#include<iostream.h>
+int main()
+{
+  //using size of function.
+  cout<<Size of char:<<sizeof(char)<<byte<<endl; 
+  cout<<Size of int:<<sizeof(int)<<bytes<<endl;
+  cout<<Size of float:<<sizeof(float)<<bytes<<endl;
+  cout<<Size of double:<<sizeof(double)<<bytes<<endl;
+  return 0;
+}
+
+/*OUTPUT
+  size of char:1byte
+  size of int:4bytes
+  size of float:4bytes 
+  size of double:8byte */ 
