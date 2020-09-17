fa77d51da4114c358918d486ac6dba878d5df128
Nil Bhingradiya <70262861+nilbhingradiya@users.noreply.github.com>
2020-09-17 11:15:02 +0530

    Create integers.cpp

---
+++ integers.cpp
@@ -0,0 +1,22 @@
+//Program to Add Two Intgers.
+
+#include<iostream.h>
+int main()
+{
+  int firstNumber,SecondNumber,sumOfTwoNumbers;
+  cout<<Enter two integers;
+  cin>>firstNumber>>secondNumber;
+  
+  /*sum of two numbers in stored in variable sumoftwonumbwers*/
+  
+  sumOfTwoNumbers=firstNumber+secondNumber;
+  
+  //print sum
+  cout<<firstNumber<<+<<secondNumber<<=<<sumOfTwoNumbers;
+  
+  return0;
+}
+
+//OUTPUT
+//Enter two integers:4 5
+//4+5=9
