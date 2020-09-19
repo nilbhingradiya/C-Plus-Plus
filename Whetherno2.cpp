573dde34462e8e4acb9911107f39e05e574d2fdc
Nil Bhingradiya <70262861+nilbhingradiya@users.noreply.github.com>
2020-09-19 09:53:44 +0530

    Create Whetherno2.cpp

---
+++ Whetherno2.cpp
@@ -0,0 +1,17 @@
+//Check whether number is even or odd using ternary operators.
+
+#include<iostream.h>
+int main()
+{
+  int n;
+  cout<<Enter an integer;
+  cin>>n;
+  (n%2==0)?cout<<n<<is even.;
+  cout<<n<<is odd.;
+  return 0;
+}
+
+/*OUTPUT
+
+  Enter an integer:13
+  13 is odd */
