8bcdba7e743af9e562b6f61e941b1a793ae9265c
Nil Bhingradiya <70262861+nilbhingradiya@users.noreply.github.com>
2020-09-19 09:49:38 +0530

    Create whetherno.cpp

---
+++ whetherno.cpp
@@ -0,0 +1,19 @@
+//Chack Whether number is even or odd using if else.
+
+#include<iostream.h>
+int main()
+{
+  int n;
+  cout<<Enter an integer:;
+  cin>>n;
+  if(n%2==0)
+      cout<<n<<is even;
+  else
+      cout<<n<<is odd;
+  return 0;
+}
+
+/*OUTPUT
+
+    Enter an integer:23
+    23 is odd */
